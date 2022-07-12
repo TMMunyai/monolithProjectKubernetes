0561b5f697ebfb7e41b79dcc25dd5ecbe8fe8770
TMMunyai <acumenone.tm@gmail.com>
2022-07-12 18:38:30 +0200

    sequelizeTroubleshootingCOmmit

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -2,12 +2,13 @@ import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
 
-export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
-
-  'dialect': config.dialect,
-  'storage': ':memory:',
-});
+const sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}/${config.database}`);
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.\n')
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err)
+  })
+  export default sequelize
